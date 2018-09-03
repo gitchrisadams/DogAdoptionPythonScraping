594beca063d4bea8bad969f2d08a31aac531b5a9
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-03 19:37:32 -0400

    Scraping of dog images of first page working

--- djangoTemplateWithAddedModule/utils/parse_scraped.py
+++ djangoTemplateWithAddedModule/utils/parse_scraped.py
@@ -1,9 +1,20 @@
+import cssutils
 from bs4 import BeautifulSoup
+
 
 def parse_html(raw_html):
     html = BeautifulSoup(raw_html, 'html.parser')
-    html_parsed = []
-    for i, li in enumerate(html.select('li')):
-        html_parsed.append(i)
-        html_parsed.append(li.text)
-    return html_parsed
+
+    div_style = html.find_all('div')
+
+    # style = cssutils.parseStyle(div_style)
+    # urlParsed = style['background-image']
+    div_list = []
+
+    for div in div_style:
+        divStr = str(div)
+        if 'image:url(' in divStr:
+            div_list.append((divStr.split('image:url(')[1]).split(')')[0])
+
+
+    return div_list
